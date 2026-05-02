248ad610a2b86152141d087e012dfab79a64ce1a
Om Patil <thatlowlifedbackbencher@gmail.com>
2026-05-02 19:51:28 +0530

    added stuff

--- src/bf_asm_be.cpp
+++ src/bf_asm_be.cpp
@@ -7,8 +7,6 @@
 
 using namespace std;
 
-
-// #define COMPILE_FOR_SYS_V 1
 #ifndef COMPILE_FOR_SYS_V
     #ifdef _WIN32
         #define COMPILE_FOR_SYS_V 0
@@ -16,46 +14,112 @@ using namespace std;
         #define COMPILE_FOR_SYS_V 1
     #endif
 #endif
+static_assert(COMPILE_FOR_SYS_V == 0 || COMPILE_FOR_SYS_V == 1, "COMPILE_FOR_SYS_V must be 0 or 1");
 
-
-
-class RegisterManager {
-public:
-    enum GPR {
-        RAX,    RBX,    RCX,    RDX,    RSI,    RDI,    RBP, /*no*/
-        R8,     R9,     R10,    R11,    R12,    R13,    R14,    R15,
-    };
-#if COMPILE_FOR_SYS_V
-    static constexpr std::array<GPR, 9> caller_saved = {
-        RAX, RCX, RDX, RSI, RDI, R8, R9, R10, R11
-    };
-    static constexpr std::array<GPR, 6> callee_saved = {
-        RBX, RBP, R12, R13, R14, R15
-    };
-    static constexpr std::array<GPR, 6> argument_reg = {
-        RDI, RSI, RDX, RCX, R8,  R9
-    };
-    static constexpr std::array<GPR, 1> return_reg = {
-        RAX
-    };
-    
-#else
-    static constexpr std::array<GPR, 7> caller_saved = {
-        RAX, RCX, RDX, R8, R9, R10, R11
-    };
-    static constexpr std::array<GPR, 8> callee_saved = {
-        RBX, RBP, RDI, RSI, R12, R13, R14, R15
-    };
-    static constexpr std::array<GPR, 4> argument_reg = {
-        RCX, RDX, R8,  R9
-    };
-    static constexpr std::array<GPR, 1> return_reg = {
-        RAX
-    };
-#endif
+// if you ever change this, change everything related to it too... or just... don't.
+enum GPR : uint8_t {
+    RAX,    RBX,    RCX,    RDX,    RSI,    RDI,    RBP, /* we do not take its name in public */
+    R8,     R9,     R10,    R11,    R12,    R13,    R14,    R15,
+    NUM_GPR
 };
+const array<const string, 15> QR = {
+    "%rax", "%rbx", "%rcx", "%rdx", "%rsi", "%rdi", "%rbp",
+    "%r8",  "%r9",  "%r10", "%r11", "%r12", "%r13", "%r14", "%r15"
+};
+const array<const string, 15> LR = {
+    "%eax", "%ebx", "%ecx", "%edx", "%esi", "%edi", "%ebp",
+    "%r8d", "%r9d", "%r10d","%r11d","%r12d","%r13d","%r14d","%r15d"
+};
+const array<const string, 15> WR = {
+    "%ax",  "%bx",  "%cx",  "%dx",  "%si",  "%di",  "%bp",
+    "%r8w", "%r9w", "%r10w","%r11w","%r12w","%r13w","%r14w","%r15w"
+};
+const array<const string, 15> BR = {
+    "%al",  "%bl",  "%cl",  "%dl",  "%sil", "%dil", "%bpl",
+    "%r8b", "%r9b", "%r10b","%r11b","%r12b","%r13b","%r14b","%r15b"
+};
+static_assert(QR.size() == NUM_GPR
+    && LR.size() == NUM_GPR
+    && WR.size() == NUM_GPR
+    && BR.size() == NUM_GPR, "invalid register name format");
 
+typedef void(*EvictFn)(void*);
 
+class RegMan {
+public:
+    // it would probably be cleaner to do this with only one #if-#else-#endif block, but my
+    // clangd, for some reason, doesn't fold preprocessor blocks, and I want these to be foldable
+    // so I'm defining them like this
+    static constexpr array<GPR, 7+2*COMPILE_FOR_SYS_V> caller_saved = {
+        RAX, RCX, RDX,
+    #if COMPILE_FOR_SYS_V
+        RSI, RDI,
+    #endif
+        R8, R9, R10, R11
+    };
+    static constexpr array<GPR, 8-2*COMPILE_FOR_SYS_V> callee_saved = {
+        RBX, RBP,
+    #if !COMPILE_FOR_SYS_V
+        RSI, RDI,
+    #endif
+        R12, R13, R14, R15
+    };
+    static constexpr array<GPR, 4+2*COMPILE_FOR_SYS_V> argument_reg = {
+    #if COMPILE_FOR_SYS_V
+        RDI, RSI, RDX, RCX,
+    #else
+        RCX, RDX,
+    #endif
+        R8,  R9
+    };
+    static constexpr array<GPR, 1> return_reg = {
+        RAX
+    };
+
+    /* We define two types of registers, Nobles, and peasants (peasants should never be capitalized
+     * even if it starts a sentence).
+     * Nobles live in the callee-saved registers. If the user attempts to allocate more Nobles than
+     * the number of caller-saved registers, `RegMan` will throw a runtime error. Nobles can only
+     * be evicted by the user.
+     * peasants can live in both the callee-saved registers or the caller-saved registers. peasants
+     * which live in caller-saved registers are called serfs. peasants must provide a priority and
+     * an eviction callback. 
+     */
+    
+    void noble_evict_fn(void* _) {
+        throw std::runtime_error("The aristocracy has fallen.");
+    }
+    struct Register {
+        GPR loc;
+        bool noble;
+        void* aux;
+        EvictFn evict;
+    };
+
+    vector<GPR> free_callee_saved;
+    vector<GPR> free_caller_saved;
+    vector<Register> occupied_registers;
+
+    inline bool is_callee_saved(GPR reg) {
+        static_assert(NUM_GPR == 15, "Invalid GPR");
+        return (0b111100001110010 & (COMPILE_FOR_SYS_V*0b111111111001111)) & (uint32_t{1} << reg);
+    }
+    inline bool is_caller_saved(GPR reg) {
+        return (0b000011110001101 | (COMPILE_FOR_SYS_V*0b000000000110000)) & (uint32_t{1} << reg);
+    }
+
+    RegMan() {
+        for(const GPR& reg : callee_saved) {
+            free_callee_saved.push_back(reg);
+        }
+        for(const GPR& reg : caller_saved) {
+            free_caller_saved.push_back(reg);
+        }
+    }
+
+    Register* allocate_noble(GPR pref);
+
+};
 
 
 
@@ -67,81 +131,26 @@ int dump_assembly(string& output_file_name, vector<command>& commands, bool assu
         return 1;
     }
 
-#if COMPILE_FOR_SYS_V
-    static const char* const prologue = R"(.text
-.globl run
+    fo << R"(.text
+.global run
 .extern putchar
 .extern getchar
 
 run:
-    # Prologue
-    push %rbx
-    push %rbp
-    push %r12
-    push %r13
-    push %r14
-    push %r15
-
-    mov %rdi, %rbx
-    mov %rsi, %rbp
-    xor %r14, %r14
-
 )";
-    static const char* const epilogue = R"(
-
-    # Epilogue
-.epilogue:
-    pop %r15
-    pop %r14
-    pop %r13
-    pop %r12
-    pop %rbp
-    pop %rbx
-    ret
+    RegMan rm;
 
 
-.section .note.GNU-stack,"",@progbits
-)";
-#else
-    static const char* const prologue = R"(.text
-.globl run
-.extern putchar
-.extern getchar
-
-run:
-    # Prologue
-    push %rbx
-    push %rbp
-    push %rdi
-    push %rsi
-    push %r12
-    push %r13
-    push %r14
-    push %r15
-
-    mov %rcx, %rbx
-    mov %rdx, %rbp
-    xor %r14, %r14
-
-)";
-    static const char* const epilogue = R"(
-
-    # Epilogue
-.epilogue:
-    pop %r15
-    pop %r14
-    pop %r13
-    pop %r12
-    pop %rsi
-    pop %rdi
-    pop %rbp
-    pop %rbx
-    ret
-)";
-#endif
+    static_assert(RegMan::callee_saved.size() % 2 == 0, "number of callee saved registers is odd");
+    for(GPR reg : RegMan::callee_saved) {
+        fo << "    push " << QR[reg] <<"\n";
+    }
+    static_assert(RegMan::argument_reg.size() >= 2, "not enough argument registers");
+    fo << "\n    mov " << QR[RegMan::argument_reg[0]] << ", " << QR[RBX]
+        << "\n    mov " << QR[RegMan::argument_reg[1]] << ", " << QR[RBP]
+        << "\n    xor " << QR[R14] << ", " << QR[R14] << "\n\n";
     
     string num;
-    fo << prologue;
     int32_t loop_id = 0;
     int32_t ncrab_id = 0;
     for(command& cmd : commands) {
@@ -196,7 +205,17 @@ run:
                 break;
         }
     }
-    fo << epilogue;
+    
+    fo << R"(
+.epilogue:
+)";
+    for(int i = RegMan::callee_saved.size()-1; i >= 0; i--) {
+        fo << "    pop " << QR[RegMan::callee_saved[i]] <<"\n";
+    }
+    fo << "    ret\n";
+#ifdef COMPILE_FOR_SYS_V
+    fo << "\n\n.section .note.GNU-stack,\"\",@progbits\n";
+#endif
 
     return 0;
 }
